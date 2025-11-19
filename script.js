19c3ee8b5f88becbfb7af0761e24b486b3ca7cb4
Lucas <ltomazmonteiro@gmail.com>
2025-11-19 20:25:20 -0300

    salvando alterações da lari

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ function abrirMenuMobile() {
 }
 
 mobileMenu.addEventListener('click', abrirMenuMobile);
+
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target); 
+      }
+    });
+  },
+  {
+    threshold: 0.2 
+  }
+);
+
+document.querySelectorAll(".reveal").forEach((el) => {
+  observer.observe(el);
+});
